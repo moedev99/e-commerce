760186ef2f6230a3ff8526400f0e60d7c3eb474b
moedev99 <mohammedshokai99@gmail.com>
2025-07-19 14:31:43 -0400

    start project

---
+++ src/test/java/com/moedev99/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moedev99.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
